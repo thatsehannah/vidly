f105dec658c6614537ea3a77b232d53ec798b8c2
Elliot Hannah <echannah631@gmail.com>
2021-02-05 15:22:22 -0800

    eager loaded discount rate of customer

--- Vidly_V2/Controllers/CustomersController.cs
+++ Vidly_V2/Controllers/CustomersController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -23,7 +24,7 @@ namespace Vidly_V2.Controllers
 
         public ActionResult Index()
         {
-            var customers = _context.Customers.ToList();
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
 
             return View(customers);
         }
