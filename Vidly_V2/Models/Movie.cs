a2e8e818f9640d06375c03764f007daf80f19fc0
Elliot Hannah <echannah631@gmail.com>
2021-02-05 16:39:31 -0800

    added release date, dateadded, numberinstock columns to movie

--- Vidly_V2/Models/Movie.cs
+++ Vidly_V2/Models/Movie.cs
@@ -13,5 +13,11 @@ namespace Vidly_V2.Models
         public string Name { get; set; }
         public Genre Genre { get; set; }
         public byte GenreId { get; set; }
+        [Required]
+        public DateTime ReleaseDate { get; set; }
+        [Required]
+        public DateTime DateAdded { get; set; }
+        [Required]
+        public byte NumberInStock { get; set; }
     }
 }
