7522fbca2cc39e3fa0ab950663610074a88d2bd1
Elliot Hannah <echannah631@gmail.com>
2021-03-01 07:49:42 -0800

    validation for saving new customers

--- Vidly_V2/Controllers/CustomersController.cs
+++ Vidly_V2/Controllers/CustomersController.cs
@@ -44,6 +44,16 @@ namespace Vidly_V2.Controllers
         [HttpPost]
         public ActionResult Save(Customer customer)
         {
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new CustomerFormViewModel
+                {
+                    Customer = customer,
+                    MembershipTypes = _context.MembershipTypes.ToList()
+                };
+                return View("CustomerForm", viewModel);
+            }
+
             if (customer.Id == 0)
             {
                 _context.Customers.Add(customer);
