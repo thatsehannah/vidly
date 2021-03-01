fb6a3907b31f83fe58ea40f9769d3526ff5c3f07
Elliot Hannah <echannah631@gmail.com>
2021-03-01 13:45:13 -0800

    created an api for movies

---
+++ Vidly_V2/Dtos/MovieDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Vidly_V2.Dtos
+{
+    public class MovieDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public byte GenreId { get; set; }
+
+        [Required]
+        public DateTime ReleaseDate { get; set; }
+
+        [Required]
+        public DateTime DateAdded { get; set; }
+
+        [Required]
+        [Range(1, 100, ErrorMessage = "The number in stock must be between 1 and 100")]
+        public byte NumberInStock { get; set; }
+    }
+}
