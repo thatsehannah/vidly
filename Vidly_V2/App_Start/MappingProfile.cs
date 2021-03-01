da3fed2da7934f7731813a2fe2722857b68c109d
Elliot Hannah <echannah631@gmail.com>
2021-03-01 12:18:51 -0800

    created customer api controller, installed automapper, created customer dto and mapping profile

---
+++ Vidly_V2/App_Start/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Vidly_V2.Dtos;
+using Vidly_V2.Models;
+
+namespace Vidly_V2.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            Mapper.CreateMap<Customer, CustomerDto>();
+            Mapper.CreateMap<CustomerDto, Customer>();
+        }
+    }
+}
--- Vidly_V2/Global.asax.cs
+++ Vidly_V2/Global.asax.cs
@@ -5,6 +5,9 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.Http;
+using AutoMapper;
+using Vidly_V2.App_Start;
 
 namespace Vidly_V2
 {
@@ -12,6 +15,8 @@ namespace Vidly_V2
     {
         protected void Application_Start()
         {
+            Mapper.Initialize(cfg => cfg.AddProfile<MappingProfile>());
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
